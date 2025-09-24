3b11ab7098bcdda2df4792563e085bd69e481ba5
rameshwar mane <manerameshwar909@gmail.com>
2025-09-24 13:06:30 +0530

    Add initial implementation of ChatOpenAI model with dotenv integration

---
+++ Langchain/2.ChatModels/1_chatmodel_demo.py
@@ -0,0 +1,11 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model='gpt-4',temperature=0)
+
+response = model.invoke("What is the capital of India?")
+print(response.content)
+
+
